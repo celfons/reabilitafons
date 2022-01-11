82018ca436a7250b01530d27712fb0a6050824a4
Marcel Fonseca Resende de Oliveira <marcel.resende@stone.com.br>
2022-01-10 22:49:50 -0300

    Add auth

--- reabilitafons/urls.py
+++ reabilitafons/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('centroherd.urls')),
     path('admin/', admin.site.urls),
+    path('account/', include('django.contrib.auth.urls')),
 ]
