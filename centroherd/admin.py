8a4bf25f773df985241cdfae57b9a62cab7e4ff5
Marcel Fonseca Resende de Oliveira <marcel.resende@stone.com.br>
2022-01-11 14:49:02 -0300

    fix

--- centroherd/admin.py
+++ centroherd/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Patients
 
 class PatientsAdmin(admin.ModelAdmin):
-    list_display  = ('name','cpf', 'birthday', 'created_at', 'updated_at', 'status', 'user')
+    list_display  = ('name','cpf', 'created_at', 'updated_at', 'status', 'user')
     search_fields = ['name', 'cpf']
 
 admin.site.register(Patients, PatientsAdmin)
--- centroherd/models.py
+++ centroherd/models.py
@@ -5,8 +5,7 @@ class Patients(models.Model):
 
     STATUS = (('active', 'Ativo'), ('inactive', 'Inativo'))
     name = models.CharField(max_length=200)
-    cpf = models.IntegerField()
-    birthday = models.DateField(auto_now_add=True)
+    cpf = models.CharField(max_length=11, unique=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     status = models.CharField(max_length=15, choices=STATUS)
