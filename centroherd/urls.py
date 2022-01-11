29eef4e3aaa3ac5c311a5a0977a11ed1f5cbeff6
Marcel Fonseca Resende de Oliveira <marcel.resende@stone.com.br>
2022-01-11 13:42:56 -0300

    add config de url

--- centroherd/urls.py
+++ centroherd/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'centroherd'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('criar/', views.patients_create, name='patients_create'),
