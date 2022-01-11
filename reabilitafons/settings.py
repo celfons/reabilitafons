7abe2fd97e65f453e17650441c0cece2b10d5de7
Marcel Fonseca Resende de Oliveira <marcel.resende@stone.com.br>
2022-01-11 12:37:12 -0300

    log true

--- reabilitafons/settings.py
+++ reabilitafons/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-68ibjpd#rsu@wri%@b#wn(irfkymqv77l@%kk=(1^-sdnvwgzz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://reabilitafons.herokuapp.com']
 
