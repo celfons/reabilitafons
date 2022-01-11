abe023e3d260c18bc31b097061cc14a57e21ea95
Marcel Fonseca Resende de Oliveira <marcel.resende@stone.com.br>
2022-01-11 13:35:28 -0300

    add models

--- centroherd/models.py
+++ centroherd/models.py
@@ -1,14 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Patients(models.Model):
- STATUS = (
-   ('active', 'Ativo'),
-   ('inactive', 'Inativo')
-  )
-  name = models.CharField(max_length=200)
-  cpf = models.IntegerField()
-  birthday = models.DateField(auto_now_add=False)
-  created_at = models.DateField(auto_now_add=True)
-  updated_at = models.DateField(auto_now=True)
-  status = models.CharField(max_length=15, choices=STATUS)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    STATUS = (('active', 'Ativo'), ('inactive', 'Inativo'))
+    name = models.CharField(max_length=200)
+    cpf = models.IntegerField()
+    birthday = models.DateField(auto_now_add=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    status = models.CharField(max_length=15, choices=STATUS)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
