10349f0599e05707ef1bdfabaafe938aba162ade
Marcel Fonseca Resende de Oliveira <marcel.resende@stone.com.br>
2022-01-12 12:31:18 -0300

    alterado campos do paciente

--- centroherd/admin.py
+++ centroherd/admin.py
@@ -1,56 +1,61 @@
 from django.contrib import admin
-from .models import Patients, Psychology, Social
+from .models import Paciente, Psicologia, Social, Contato
 from django_admin_inline_paginator.admin import TabularInlinePaginated
 
-class PsychologyInline(TabularInlinePaginated):
-    model = Psychology    
+class PsicologiaInline(TabularInlinePaginated):
+    model = Psicologia    
     per_page = 1
 
 class SocialInline(TabularInlinePaginated):
     model = Social    
     per_page = 1
 
-class PatientsAdmin(admin.ModelAdmin):
-    list_display  = ('name','cpf', 'created_at', 'updated_at', 'status', 'user')
-    search_fields = ['name', 'cpf']
+class ContatoInline(TabularInlinePaginated):
+    model = Contato    
+    per_page = 1
 
-    model = Patients
+class PacienteAdmin(admin.ModelAdmin):
+    list_display  = ('nome','documento', 'telefone', 'nascimento', 'estado_civil', 'cor', 'filhos', 'profissao', 'escolaridade', 'pai', 'mae', 'naturalidade', 'endereco', 'bairro', 'cidade', 'bairro', 'cep', 'encaminhamento', 'inss', 'email', 'criado', 'atualizado', 'status', 'usuario')
+    search_fields = ['nome', 'documento', 'status']
+
+    model = Paciente
     inlines = [
-        PsychologyInline,
+        ContatoInline,
+        PsicologiaInline,
         SocialInline,
     ]
 
     def save_model(self, request, obj, form, change): 
-        obj.user = request.user
+        obj.usuario = request.user
         obj.save()
 
-admin.site.register(Patients, PatientsAdmin)
+admin.site.register(Paciente, PacienteAdmin)
 
-class PsychologyAdmin(admin.ModelAdmin):
-    list_display  = ('name', 'text', 'created_at', 'updated_at', 'user')
-    search_fields = ['patients__name']
-    autocomplete_fields = ['patients']
-    readonly_fields=('user',)
+class PsicologiaAdmin(admin.ModelAdmin):
+    list_display  = ('nome', 'acompanhamento', 'criado', 'atualizado', 'usuario')
+    search_fields = ['paciente__nome']
+    autocomplete_fields = ['paciente']
+    readonly_fields=('usuario',)
 
     @admin.display
-    def name(self, obj):
-        return obj.patients.name
+    def nome(self, obj):
+        return obj.Paciente.nome
 
     def save_model(self, request, obj, form, change): 
         obj.user = request.user
         obj.save()
 
-admin.site.register(Psychology, PsychologyAdmin)
+admin.site.register(Psicologia, PsicologiaAdmin)
 
 class SocialAdmin(admin.ModelAdmin):
-    list_display  = ('name', 'text', 'created_at', 'updated_at')
-    search_fields = ['patients__name']
-    autocomplete_fields = ['patients']
-    readonly_fields=('user',)
+    list_display  = ('nome', 'acompanhamento', 'criado', 'atualizado', 'usuario')
+    search_fields = ['paciente__nome']
+    autocomplete_fields = ['paciente']
+    readonly_fields=('usuario',)
 
     @admin.display
-    def name(self, obj):
-        return obj.patients.name
+    def nome(self, obj):
+        return obj.Paciente.nome
 
     def save_model(self, request, obj, form, change): 
         obj.user = request.user
--- centroherd/models.py
+++ centroherd/models.py
@@ -2,36 +2,66 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 
-class Patients(models.Model):
+class Paciente(models.Model):
 
-    STATUS = (('active', 'Ativo'), ('inactive', 'Inativo'))
-    name = models.CharField(max_length=200)
-    cpf = models.IntegerField(unique=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    STATUS = (('ativo', 'Ativo'), ('inativo', 'Inativo'))
+    ESTADO = (('solteiro', 'Solteiro'), ('casado', 'Casado'), ('viuvo', 'Viuvo'), ('divorciado', 'Divorciado'))
+    ESCOLARIDADE = (('analfabeto', 'Analfabeto'), ('infantil', 'Infantil'), ('primario', 'Primario'), ('fundamental', 'Fundamental'), ('medio', 'Medio'), ('superior', 'Superior'), ('desconhecido', 'Desconhecido'))
+    nome = models.CharField(max_length=200)
+    documento = models.IntegerField(unique=True)
+    telefone = models.IntegerField(blank=True, null=True)
+    nascimento = models.DateField(auto_now_add=False, blank=True, null=True)
+    estado_civil = models.CharField(max_length=15, choices=ESTADO, blank=True)
+    cor = models.CharField(max_length=20, blank=True)
+    filhos = models.BooleanField(null=True)
+    profissao = models.CharField(max_length=100, blank=True)
+    escolaridade = models.CharField(max_length=15, choices=ESCOLARIDADE, blank=True)
+    pai = models.CharField(max_length=200, blank=True)
+    mae = models.CharField(max_length=200, blank=True)
+    naturalidade = models.CharField(max_length=50, blank=True)
+    endereco = models.CharField(max_length=50, blank=True)
+    bairro = models.CharField(max_length=50, blank=True)
+    cidade = models.CharField(max_length=50, blank=True)
+    cep = models.IntegerField(blank=True, null=True)
+    encaminhamento = models.CharField(max_length=200, blank=True)
+    inss = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(blank=True)
+    criado = models.DateField(auto_now_add=True)
+    atualizado = models.DateField(auto_now=True)
     status = models.CharField(max_length=15, choices=STATUS)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
-    class Meta:
-        verbose_name = ("Paciente")
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+
     def __str__(self):
-        return self.name
+        return self.nome
 
-class Psychology(models.Model):
+class Contato(models.Model):
 
-    patients = models.ForeignKey(Patients, on_delete=models.CASCADE)
-    text = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200)
+    parentesco = models.CharField(max_length=100)
+    telefone = models.IntegerField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    criado = models.DateField(auto_now_add=True)
+    atualizado = models.DateField(auto_now=True)
+    class Meta:
+        verbose_name = ("Telefones de Contato")
+
+class Psicologia(models.Model):
+
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    acompanhamento = models.TextField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    criado = models.DateField(auto_now_add=True)
+    atualizado = models.DateField(auto_now=True)
     class Meta:
         verbose_name = ("Psicologia - Prontuario")
 
 class Social(models.Model):
 
-    patients = models.ForeignKey(Patients, on_delete=models.CASCADE)
-    text = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    acompanhamento = models.TextField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    criado = models.DateField(auto_now_add=True)
+    atualizado = models.DateField(auto_now=True)
     class Meta:
         verbose_name = ("Serviço Social - Prontuario")
