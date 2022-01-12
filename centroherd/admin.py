665e0014de4712f5972befef6b4ae348530237f7
Marcel Fonseca Resende de Oliveira <marcel.resende@stone.com.br>
2022-01-12 13:23:44 -0300

    fix para salvar form model

--- centroherd/admin.py
+++ centroherd/admin.py
@@ -5,18 +5,22 @@ from django_admin_inline_paginator.admin import TabularInlinePaginated
 class PsicologiaInline(TabularInlinePaginated):
     model = Psicologia    
     per_page = 1
+    readonly_fields=('usuario',)
 
 class SocialInline(TabularInlinePaginated):
     model = Social    
     per_page = 1
+    readonly_fields=('usuario',)
 
 class ContatoInline(TabularInlinePaginated):
     model = Contato    
     per_page = 1
+    readonly_fields=('usuario',)
 
 class PacienteAdmin(admin.ModelAdmin):
     list_display  = ('nome','documento', 'telefone', 'nascimento', 'estado_civil', 'cor', 'filhos', 'profissao', 'escolaridade', 'pai', 'mae', 'naturalidade', 'endereco', 'bairro', 'cidade', 'bairro', 'cep', 'encaminhamento', 'inss', 'email', 'criado', 'atualizado', 'status', 'usuario')
     search_fields = ['nome', 'documento', 'status']
+    readonly_fields=('usuario',)
 
     model = Paciente
     inlines = [
@@ -28,6 +32,11 @@ class PacienteAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change): 
         obj.usuario = request.user
         obj.save()
+
+    def save_formset(self, request, form, formset, change):
+        for form in formset.forms:
+            form.instance.usuario = request.user
+        formset.save()
 
 admin.site.register(Paciente, PacienteAdmin)
 
